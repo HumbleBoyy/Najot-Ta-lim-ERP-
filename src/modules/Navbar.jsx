c7c6deb93ec84b3ea3a226eb35c833bdb248cb97
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-02-02 12:25:55 +0500

    New Commit

--- src/modules/Navbar.jsx
+++ src/modules/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='w-[20%] h-[100vh] overflow-y-auto'>
+    <div className='w-[20%] bg-blue-600 h-[100vh] overflow-y-auto'>
       Navbar
     </div>
   )
